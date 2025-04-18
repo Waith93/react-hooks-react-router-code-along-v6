acb29dac48f5206994c9722f71aabace8871e3fb
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-04-19 00:46:28 +0300

    router code along

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar";
+
+function Login() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Login Page</h1>
+        {/* You can add the login form or other elements here */}
+      </main>
+    </>
+  );
+};
+
+export default Login;
